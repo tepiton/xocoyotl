e38bf71b38fbff094b0051a8a98d4c9b71dbddbc
Philip Borenstein <pborenstein@gmail.com>
2021-11-02 05:26:00 -0400

    wip
    note posted to src/posts: Thu Oct 28 2021 22:56:08 GMT-0400 (EDT)
    wip
    
    note deleted from src/posts: Thu Oct 28 2021 23:04:24 GMT-0400 (EDT)
    note posted to src/posts: Thu Oct 28 2021 23:33:12 GMT-0400 (EDT)
    markdown the titles
    
    wipe down the counters
    
    note posted to src/posts: Fri Oct 29 2021 12:40:30 GMT-0400 (EDT)
    note deleted from src/posts: Fri Oct 29 2021 20:19:02 GMT-0400 (EDT)
    wip
    
    note deleted from src/posts: Fri Oct 29 2021 20:22:32 GMT-0400 (EDT)
    note posted to src/posts: Fri Oct 29 2021 20:35:21 GMT-0400 (EDT)
    note posted to src/posts: Fri Oct 29 2021 20:36:25 GMT-0400 (EDT)
    note posted to src/posts: Fri Oct 29 2021 20:37:43 GMT-0400 (EDT)
    note posted to src/posts: Fri Oct 29 2021 23:02:16 GMT-0400 (EDT)
    note posted to src/posts: Sun Oct 31 2021 11:22:04 GMT-0400 (EDT)
    collections are gone
    
    note posted to src/posts: Mon Nov 01 2021 16:23:23 GMT-0400 (EDT)
    note posted to src/posts: Mon Nov 01 2021 21:42:50 GMT-0400 (EDT)
    i suck at css
    
    note posted to src/posts: Tue Nov 02 2021 05:01:18 GMT-0400 (EDT)
    note posted to src/posts: Tue Nov 02 2021 05:02:41 GMT-0400 (EDT)
    note posted to src/posts: Tue Nov 02 2021 05:04:01 GMT-0400 (EDT)
    note posted to src/posts: Tue Nov 02 2021 05:05:15 GMT-0400 (EDT)
    note deleted from src/posts: Tue Nov 02 2021 05:07:19 GMT-0400 (EDT)
    note deleted from src/posts: Tue Nov 02 2021 05:07:51 GMT-0400 (EDT)

--- src/index.11ty.js
+++ src/index.11ty.js
@@ -9,31 +9,6 @@ module.exports = class Home {
     }
   }
 
-  getXSummary(post) {
-    let retval = 'oops'
-    let summary = post.data.excerpt
-
-//      console.log('--gsPost', post)
-
-    let re = /<img\s+class="tepiton"\s+src="([^"]+)"/
-    let m = post.templateContent.match(re)
-
-      //  if the summary is empty use stars
-      //  if the summary is '::' use the first img we find
-      //  otherise treat it as markdown
-
-    if (! summary ) {
-      retval = stars
-    } else if (m && summary.startsWith('::')) {
-      summary = this.markdown2(summary.slice(2))
-      retval = `<img src="${m[1]}">${summary}`
-    } else {
-      retval = this.markdown(summary)
-    }
-
-    return retval
-  }
-
   getSummary(post) {
     let retval = post.data.excerpt || 'oops'
 
@@ -55,18 +30,22 @@ module.exports = class Home {
     }
 
     let noTitle = false
-    let body = ''
-    let header  = post.data.title && `<header><a href="${post.url}">${post.data.title}</a></header>`
+    let header  = post.data.title && `<header><a href="${post.url}">${this.markdown(post.data.title)}</a></header>`
     let section = `<section><a href="${post.url}">${summary}</section></a>`
     let footer  = `<footer><a href="${post.data.draft}"># ${post.date.toDateString()}</a></footer>`
 
+      //  http://svgicons.sparkk.fr/
+
     if (post.data.title === stars) {
-      header = ``
+      header = `<span class="noTitle"><a href="${post.url}"><svg class="svg-icon" viewBox="0 0 20 20">
+							<path d="M16.469,8.924l-2.414,2.413c-0.156,0.156-0.408,0.156-0.564,0c-0.156-0.155-0.156-0.408,0-0.563l2.414-2.414c1.175-1.175,1.175-3.087,0-4.262c-0.57-0.569-1.326-0.883-2.132-0.883s-1.562,0.313-2.132,0.883L9.227,6.511c-1.175,1.175-1.175,3.087,0,4.263c0.288,0.288,0.624,0.511,0.997,0.662c0.204,0.083,0.303,0.315,0.22,0.52c-0.171,0.422-0.643,0.17-0.52,0.22c-0.473-0.191-0.898-0.474-1.262-0.838c-1.487-1.485-1.487-3.904,0-5.391l2.414-2.413c0.72-0.72,1.678-1.117,2.696-1.117s1.976,0.396,2.696,1.117C17.955,5.02,17.955,7.438,16.469,8.924 M10.076,7.825c-0.205-0.083-0.437,0.016-0.52,0.22c-0.083,0.205,0.016,0.437,0.22,0.52c0.374,0.151,0.709,0.374,0.997,0.662c1.176,1.176,1.176,3.088,0,4.263l-2.414,2.413c-0.569,0.569-1.326,0.883-2.131,0.883s-1.562-0.313-2.132-0.883c-1.175-1.175-1.175-3.087,0-4.262L6.51,9.227c0.156-0.155,0.156-0.408,0-0.564c-0.156-0.156-0.408-0.156-0.564,0l-2.414,2.414c-1.487,1.485-1.487,3.904,0,5.391c0.72,0.72,1.678,1.116,2.696,1.116s1.976-0.396,2.696-1.116l2.414-2.413c1.487-1.486,1.487-3.905,0-5.392C10.974,8.298,10.55,8.017,10.076,7.825"></path>
+						</svg></a></span>`
       footer = ``
+      header = ``
       noTitle = true
     }
 
-    body =
+    let body =
     `     <article ${noTitle ? 'class="noTitle"' : ''}>
             ${header}
             ${section}
@@ -82,10 +61,10 @@ module.exports = class Home {
     let posts = data.collections.tepiton.slice()
 
     let head = `<h1 class="logo">${ data.config.siteName }</h1>`
-    let body = `
-<div class="tepiton">
-  ${posts.reverse().map(post => this.renderPost(post)).join("\n")}
-</div>`
+    let body =
+    `   <div class="tepiton">
+          ${posts.reverse().map(post => this.renderPost(post)).join("\n")}
+        </div>`
 
     return head + body
   }
